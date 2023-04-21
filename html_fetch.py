02abadfe29c83c9a6e915c3cdd14b04451f7fe91
Coen <chmeintjes@gmail.com>
2023-04-21 14:27:08 +0200

    Major update to the script
    The original idea was to extract and store relevant property attributes
    in SQLite DB but it was easier to pull the data into a dict() which
    could then be stored in json format in the DB
    
    The idea is to then pull from this DB for the property anlysis using
    pandas to create a Dataframe and then do the calculations there

--- html_fetch.py
+++ html_fetch.py
@@ -4,34 +4,73 @@
 
 import sqlite3
 from bs4 import BeautifulSoup
+import json
 
 conn = sqlite3.connect('parsed_links.sqlite')
 cur = conn.cursor()
 
-# Create the tables using executescript()
-cur.executescript('''
-DROP TABLE IF EXISTS Properties;
+all_properties = {}
+# input how many listing to process
+many = 0
+while True:
+    if ( many < 1 ) :
+        sval = input('How many pages:')
+        if ( len(sval) < 1 ) : break
+        many = int(sval)
+    many = many - 1
 
-CREATE TABLE Properties (
-    id            INTEGER PRIMARY KEY UNIQUE,
-    list_number   TEXT
-)
-''')
+    # Fetch html from the DB parse and extract values
+    cur.execute('SELECT id,html FROM Urls WHERE parsed is NULL ORDER BY RANDOM() LIMIT 1')
+    
+    # iterate through the rows and extract data
+    for row in cur:
+        id = row[0]
+        html = str(row[1])
+        soup = BeautifulSoup(html, "html.parser")
 
-# Fetch a link from the DB and follow the link
-cur.execute('SELECT html FROM Urls')
-# url = list()
-for row in cur:
-    html = str(row[0])
-    soup = BeautifulSoup(html, "html.parser")
+        # parse relevant property information located in property overview table with class='row p24_propertyOverviewRow'     
+        # Find all div elements with class "row p24_propertyOverviewRow"
+        table_rows = soup.find_all('div', class_='row p24_propertyOverviewRow') # this creates a list from each row of the table
 
-    # use relative xPath to find listing number.
-    element = soup.find(
-        'div', class_='row p24_propertyOverviewRow').find('div', 
-               class_='col-6 noPadding').find('div', 
-               class_='p24_info')
-    list_number = element.text
-cur.execute('''INSERT OR IGNORE INTO Properties (list_number)
-                            VALUES ( ? )''', ( list_number, ) )
-conn.commit()
-print(list_number)
+        property_specs = {}
+        # Iterate through the list of rows
+        for row in table_rows:
+            # Find the div tag with class "p24_propertyOverviewKey" inside the row
+            key_div = row.find('div', class_='p24_propertyOverviewKey')
+
+            if key_div:
+                # Extract the text inside the key_div
+                key_text = key_div.text.strip()
+                # print('Key:', key_text)
+            else:
+                print('Key not found.')
+            
+            # Find the div tag with class "p24_propertyOverviewKey" inside the row
+            val_div = row.find('div', class_='p24_info')
+            
+            if val_div == None:
+                continue
+            elif val_div: # Extract the text inside the val_div
+                val_text = val_div.text.strip()
+                # print('Value:', val_text)
+            else:
+                print('Value not found.')
+            property_specs[key_text] = val_text
+
+            # Get the 'Listing Number' from property_specs
+            property_id = property_specs.get('Listing Number')
+
+            # If property_id is not in all_properties, create an empty list
+            if property_id not in all_properties:
+                all_properties[property_id] = property_specs
+
+        # store the property specs in SQLite DB in json format
+        cur.execute('''UPDATE Urls SET json = ?, parsed = ? WHERE id = ? ''', ( str(property_specs), 1, id, ))
+        conn.commit()   
+
+# print(property_specs)   # Print the final property_specs dictionary after the loop
+print(json.dumps(all_properties, indent=4))  # Print the final all_properties dictionary after the loop
+
+# Save the all_properties dictionary as a JSON file
+with open('all_properties.json', 'w') as json_file:
+    json.dump(all_properties, json_file)
