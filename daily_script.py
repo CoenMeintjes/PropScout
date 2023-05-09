e8d1a8b1677c623bdfdf48debaa80b8c9cf70481
Coen <chmeintjes@gmail.com>
2023-05-09 11:06:38 +0200

    Added a code for a log file to track script runs

--- daily_script.py
+++ daily_script.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+import datetime
 import subprocess
 
-subprocess.run(['python','gmail_parse.py'])
+# Set the log file name
+log_file_name = 'daily_run_log.txt'
+
+# Open the log file for appending
+with open(log_file_name, 'a') as log_file:
+    # Get the current date and time
+    now = datetime.datetime.now()
+
+    # Write the date and time to the log file
+    log_file.write('Script run at {:%Y-%m-%d %H:%M:%S}\n'.format(now))
+
+    # Run the daily scripts
+    subprocess.run(['python','gmail_parse.py'])
+
+    # Write the end of the log entry to the log file
+    log_file.write('Script run complete.\n\n')
